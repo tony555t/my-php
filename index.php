180265ee0a78132a20adf6f34c82b57b8a3dc26f
tony555t <tonytullah764@gmail.com>
2023-10-16 09:53:25 +0300

    Initial commit

--- index.php
+++ index.php
@@ -1,365 +1,342 @@
 <?php
-//number
+class Person {
+    public $name;
+     public $surname;
+   private $age;
+    
+    
+     public function  __constructer($name,$surname)
+ {
+       $this->name=$name;
+       $this->surname=$surname;
+      }
+public function setAge($age){
+        $this->age=$age;
+    }
+}
 
+ //Creating a new instance/object
+ $p1 = new Person('antony','tula');
+ $p1->setAge(30);
 
-//declaring number
-//$a=1;
-//$b=4;
-//$c=7.8;
-//arthmetic operation
-//echo ($a+$b)*$c;
-//echo $a % $b;
-
-//assignment with math operator
-//$a=$a+$b;
- //$a+=$b; echo $a.'<br>';
- //$a-=$b;echo $a.'<br>';
- //$a*=$b;  echo $a .'<br>' ;
- //$a/=$b;$a++; echo $a.'<br>'; 
-  //post increment
- //echo++$a;
-// decrement operator
-//echo $a--.'<br>';
-//echo --$a.'<br>';
-//echo --$b.'<br>';
-//echo $a++ .'<br>';
-//echo ++$a.'<br>';
-
-// number checking function
-//is_float(1.25);
-//is_double(1.25);
-//is_int(5);
-//is_numeric('3.14');
-//is_numeric('3h.14');
-
-//conversion
-
-//$strNumber ='12.34';
-//$number =(float)$strNumber;
-//var_dump($number);
+// // Setting properties
+// $p1->name = "Alice";
+// $p1->surname = "Smith";
+// $p1->age = 30;
+//  $p2= new person ();
+//  $p2->name = "kim";
+// $p2->surname = "uyn";
+// $p2->age = 34;
+ 
 
 
 
-//$strNumber ='12.34';
-//$number =(int)$strNumber;
-//var_dump($number) .'<br>';
+// // Invoking the greet method on the instance
 
-//$strNumber ='12.34';
-//$number =intval($strNumber);
-//var_dump($number) .'<br>';
-
-//$strNumber ='12.34';
-//$number =floatval($strNumber);
-//var_dump($number) .'<br>';
-//echo '<br>';
-
-
-//echo "abs(-15) " . abs(-15) . '<br>';
-//echo "pow(2,  3) " . pow(2, 3) . '<br>';
-//echo "sqrt(16) " . sqrt(16) . '<br>';
-//echo "max(2, 3) " . max(2, 3) . '<br>';
-//echo "min(2, 3) " . min(2, 3) . '<br>';
-//echo "round(2.4) " . round(2.4) . '<br>';
-//echo "round(2.6) " . round(2.6) . '<br>';
-//echo "floor(2.6) " . floor(2.6) . '<br>';
-//echo "ceil(2.4) " . ceil(2.4) . '<br>';
-
-//number formating
-//$number=123456789.12345;
-//echo number_format($number,2,'.',',').'<br>';
-
-
-
-// Create simple string
-//$name ='zura';
-//$string ='helo i am $name.i am 28';
-//$string2="helo i am $name .iam 28";
-//echo $string.'<br>';
-//echo $string2.'<br>';
-
-
-// String concatenation
-//$country= 'usa';
-//$gunlaw ="$country has enacted the gun law";
-//echo "$gunlaw <br>";
+// echo "<br>" . $p1->greet();
+// echo "<br>" . $p2->greet();
+// echo "<br>" . $p->greet();
 
 
 
 
-// String functions
-//$word =" hello world";
-//echo "1 - " . strlen($word) . '<br>' . PHP_EOL;
-//echo "2 - " . trim($word) . '<br>' . PHP_EOL;
-//echo "3 - " . ltrim($word) . '<br>' . PHP_EOL;
-//echo "4 - " . rtrim($word) . '<br>' . PHP_EOL;
-//echo "5 - " . str_word_count($word) . '<br>' . PHP_EOL;
-//echo "6 - " . strrev($word) . '<br>' . PHP_EOL;
-//echo "7 - " . strtoupper($word) . '<br>' . PHP_EOL;
-//echo "8 - " . strtolower($word) . '<br>' . PHP_EOL;
-//echo "9 - " . ucfirst('hello') . '<br>' . PHP_EOL;
-//echo "10 - " . lcfirst('HELLO') . '<br>' . PHP_EOL;
-//echo "11 - " . ucwords('hello world') . '<br>' . PHP_EOL;
-//echo "12 - " . strpos($word, 'world') . '<br>' . PHP_EOL; // Change into world
-//echo "13 - " . stripos($word, 'world') . '<br>' . PHP_EOL;
-//echo "14 - " . substr($word, 8) . '<br>' . PHP_EOL;
-//echo "15 - " . str_replace('World', 'PHP', $word) . '<br>' . PHP_EOL;
-//echo "16 - " . str_ireplace('world', 'PHP', $word) . '<br>' . PHP_EOL;
+class Car {
+    public $name;
+    public $color;
+    public $price;
+    private $condition;
+
+    public function vehicle($name,$color,$price)
+
+{
+      $this->name=$name;
+      $this->color=$color;
+      $price->price=$price;
+
+    }
 
 
+   public function setCondition($condition)
+   
+   {
+     $this->condition=$condition;
+  }
+}
+
+// 
+//Setting properties
+$c1 =new Car ('mazda','blue','3000usd');
+$c1->setCondition('mid');
+
+// $c1->name = "toyota";
+// $c1->color = "blue";
+// $c1->price = 30000000;
+//  $c2= new Car ();
+//  $c2->name = "porshe";
+// $c2->color = "black";
+// $c2->price = 30000004;
+// // 
+// // 
+// echo '<br>'. $c1->vehicle()."<br>";
+// echo '<br>'. $c2->vehicle()."<br>";
 
 
-// Multiline text and line breaks
-
-//$longText ="hellow ,my name is ryan 
-//I am 27
-//,I love my daughter";
-//echo $longText.'<br>';
-
-
-//$longSentence ="kenya is one big country that has a huge christian population";
-//echo $longSentence.'<br>';
-// Multiline text and reserve html tags
-//$longText="
-//Hello, my name is <b>Zura</b>
-//I am <b>27</b>,
-//I love my daughter
-//";
-//echo "1 - " . $longText . '<br>';
-//echo "2 - " . nl2br($longText) . '<br>';
-//echo "3 - " . htmlentities($longText) . '<br>' . PHP_EOL;
-//echo "4 - " . nl2br(htmlentities($longText)) . '<br>' . PHP_EOL;
-//echo "5 - " . html_entity_decode(htmlentities($longText)) . '<br>' . PHP_EOL;
-//echo "6 - " . htmlspecialchars($longText) . '<br>' . PHP_EOL;
-
-
-// https://www.php.net/manual/en/ref.strings.php
-// Create array
-//$fruits=["banana","apple","orange"];
-//$money=['pesos','shiling','dollar'];
-
-// Print the whole array
-//echo'<pre>';
-//var_dump($fruits);
-//echo'<pre>';
-// Get element by index
-//echo $fruits[2].'<br>';
-// Set element by index
-///echo $fruits[0] ='peach';
-//echo $fruits[1]='mango';
-//echo $fruits[2]='pineaple';
-//echo'<pre>';
-//var_dump($fruits);
-//echo'<pre>';
-
-// Check if array has element at index 2
-//isset($fruits[3]); //false
-//isset($fruits[2]);//true
-//isset ($fruits[1]);//true
-//isset($fruits[0]);// true
-
-
-// Append element
-//$fruits[]='grapes';
-//$fruits[]='berries';
-///$fruits[]='bananas';
-//e   cho'<pre>';
-//var_dump($fruits);
-// echo'<pre>';
-// echo $fruits[3].'<br>';
-
-
-// Print the length of the array
-// echo count ($fruits).'<br>';
-// unset($fruits[3]);
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-// // Add element at the end of the array
-
-// array_push($fruits,'avocado');
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-// // Remove element from the end of the array
-// echo array_pop($fruits);
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-// echo array_pop($fruits);
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-
-// // Add element at the beginning of the array
-// array_unshift($fruits,'bar');
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-// array_unshift($fruits,'papaya');
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-// array_unshift($fruits,'kiwi');
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-
-
-// // Remove element from the beginning of the array
-// array_shift($fruits);
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
-
-// // Split the string into an array
-// $string='dragon fruit ,watermelon ,pasion fruit';
-// echo '<pre>';
-// var_dump(explode(',',$string));
-// echo'<pre>';
-// $string3='pink,green,blue,red';
-// echo '<pre>';
-// var_dump(explode(',',$string3));
-// echo'<pre>';
-// $counties='kenya, uganda, tz, somalia';
-// echo '<pre>';
-// var_dump(explode(',',$counties));
-// $teams='arsenal,man u,liverpool,mancity,briton';
-// echo'<pre>';
-// var_dump (explode(',', $teams ));
-// echo'<pre>';
-
-
-// // Combine array elements into string
-// $fruits=["banana","apple","orange"];
-// echo'<pre>';
-// var_dump (implode(",", $fruits));
-// echo'<pre>';
-// // Check if element exist in the array
-
-// //var_dump (in_array("arsenal", $teams));
-
-
-// // Search element index in the array
-// echo'<pre>';
-// var_dump(array_search('melon
-// ',$fruits));
-// echo'<pre>';
-
-// // Merge two arrays
-// $vegetables = ['potato','cucumber'];
-// echo'<pre>';
-// var_dump(array_merge($vegetables,$fruits));
-// var_dump([$fruits,$vegetables]);
-
-// echo'</pre>';
-
-
-// // Sorting of array (Reverse order also)
-// sort($fruits);
-// echo'<pre>';
-// var_dump($fruits);
-// echo'</pre>';
+class Course { 
+    public $name;
+    public $duration;
+    public $fees;
+  private $adm_no;
   
 
-// https://www.php.net/manual/en/ref.array.php
+    public function __constructer($name,$duration,$fees)
+{
+       $this->name=$name;
+        $this->duration=$duration;
+        $this->fees=$fees;
+    }
+ public function setAdm_no($adm_no)
+{
+    
+   $this->adm_no=$adm_no;
+}
+    }
+    // create an instant
+     $l1 = new Course('ui/ux','6months',100000);
+   $li->setAdm_no(7865);
 
-// ============================================
-// Associative arrays
-// ============================================
+//     $l1->name="php programming";
+//     $l1->duration ="6 months ";
+//     $l1->fees =50000;
+//     $l2 =new Course();
+//     $l2->name='java programming ';
+//     $l2->duration='8 months';
+//     $l2->fees=1000000;
 
-// Create an associative array
-// $person=[
-//     'name'=>'brad',
-//     'surname'=>'junior',
-//     'age'=> 40,
-//     'hobbies'=>['tenis','code']];
-//     echo '<pre>';
-//     var_dump($person);
-//     echo'</pre>';
+//     echo '<br>'. $l1->learn().'<br>';
+//  echo '<br>'.$l2->learn ().'<br>';
+
+ class Animal{
+    public $name;
+    public $color;
+    public $age;
+  private $health_status;
 
 
+    public function __constructor($name,$color,$age)
+{
+         $this->name=$name;
+          $this->color=$color;
+          $this->age=$age;
+    }
+public  function setHealth_status($health_status)
+{
+    $this->health_status=$health_status;
+}
+ }
+//create an instance 
+$an1=new Animal('cat','brown','2yr');
+$an1->setHealth_status('sick');
+// $an1->color='brown:';
+// $an1->age=7;
+
+// $an2=new Animal();
+// $an2->name='buffalo:';
+// $an2->color='brown:';
+// $an2->age=10;
+
+// echo '<br>'. $an1->wild().'<br>';
+// echo '<br>'.$an2->wild ().'<br>';
 
 
+class Hotel{
+    public $name ;
+    public $location ;
+    public $capacity ;
+   private $no_employee;
 
+    public function __constructor($name,$location,$capacity)
+{
+         $this->name=$name;
+          $this->location=$location;
+          $this->capacity=$capacity;
+    }
+ public function setNo_employee($no_employee)
+ {
+    $this->no_employee = $no_employee;
+ }
 
-// // Get element by key
-// echo "<p>".$person["name"]."</p>";
-// echo '<p>'.$person ['age'].'</p>';
-// echo '<p>'.$person ['hobbies'],'</p>';
+}
+//create an instance 
+$h1=new hotel('utali','thika', 100);
+$h1->setNo_employee(35);
 
-// // Set element by key
-// //$person['location']='nairobi kenya';
-// //echo '<pre>';
-// //var_dump ($person );
-// //echo '</pre>';
+// $h1->location='Nairobi';
+// $h1->capacity="30";
+// $h2= new hotel ();
+// $h2->name='Lake plaza';
+// $h2->location='Mombasa';
+// $h2->capacity ="50 ";
 
-// // Null coalescing assignment operator. Since PHP 7.4
-//  if (!isset($person ['location'])){    
-//     $person['location']=null;
+// // Outputting vehicle information
+// echo '<br>'.$h1->find().'<br>';
 
-//  }
-// //  echo '<pre>';
-// // var_dump ($person );
-// // echo '</pre>';
+class School{
+    public $name;
+    public $location;
+    public $population;
+private $no_teachers;
 
-// $person['location'] ??='unknown';
-// echo'<pre>';
-// var_dump($person);
-// echo'</pre>';
-// // Check if array has specific key
+    public function __construct($name ,$location,$population)
+    {
+     $this->name=$name;
+     $this->location=$location;
+     $this->population=$population;
+    }
+  public function setNo_teachers($no_teachers){
+   $this->no_teachers=$no_teachers;
+
+ }
+}
+$s = new School("lenana",'kiambu', 1200);
+$s->setNo_teachers('20');
 // echo '<pre>';
-// var_dump(array_keys($person));
+// var_dump($p);
 // echo '</pre>';
-// // Print the keys of the a
-
-// // Print the values of the array
-// echo '<pre>';
-// var_dump(array_values($person));
-// echo '</pre>';
-
-// // Sorting associative arrays by values, by keys
-// asort($person);
-// echo'<pre>';
-// var_dump($person);
-// echo '</pre>';
-
-// // Two dimensional arrays
-// $todo=[
-//     ['title'=>'Todo title 1','completed'],
-//     ['title'=>'Todo title 2','completed']
-
-// ];
-// echo'<pre>';
-// var_dump($todo);
-// echo '</pre>';
-$age = 20;
-$salary = 300000;
-
-// Sample if
+// ;$s2 = new School( "kahawa",'juja',1500);
 
 
 
 
-// Without circle braces
+class Saccos{
+    //identify properties
+    public $name;
+    public $route;
+    public $color;
+    private $owner;
+     //create a constructed
+     public function __construct($name,$route,$color){
+        $this->name=$name;
+        $this->route=$route;
+        $this->color=$color;
 
-// Sample if-else
+            }
+            //set the property
+            public function setOwner($owner){
+                $this->owner=$owner;
 
-// Difference between == and ===
-
-// if AND
-
-// if OR
-
-
-
-
-// Ternary if
-
-// Short ternary
-
-// Null coalescing operator
-
-// switch
+            }
 
 
-?>
+  
+
+
+}
+//instancess creation
+$sc1= new saccos("rembo","kiambu","green");
+$sc1->setOwner('kevin mwangi');
+
+// $sc1->name='twin star';
+// $sc1->route='kabete';
+// $sc1->color='blue';
+// $sc2= new saccos ();
+// $sc2->name='umoinner';
+// $sc2->route='umoja';
+// $sc2->color ="red ";
+
+// ///out put the sacco
+
+// echo '<br>'.$sc1->travel().'<br>';
+// echo '<br>'.$sc2->travel().'<br>';
+
+
+class Iphone{
+    public $name;
+    public $year_of_release;
+    private $serial_no;
+
+    public function  __construct($name,$year_of_release)
+    {
+        $this->name=$name;
+        $this->year_of_release=$year_of_release;
+        
+        
+    }
+     
+       public function setSerial_no( $serial_no)
+       {
+        $this ->serial_no=$serial_no;
+       }
+
+
+    }
+
+
+
+//create instances
+$ip=new Iphone("Iphone x","2018");
+$ip->setSerial_no('1234wgytdf');
+echo'<br>';
+var_dump ($ip);
+echo'</br>';
+
+// $ip->name="Iphone x";
+// $ip->year_of_release = "2018";
+
+// $ip2=new Iphone();
+// $ip2->name='iphone 12';
+// $ip2->year_of_release ='2021';
+
+// $ip3= new iphone();
+// $ip3->name='iphone 7';
+// $ip3->year_of_release='2015 ';
+
+
+// $ip4= new Iphone();
+// $ip4->name='iphone XR';
+// $ip4->year_of_release='2020';
+
+// echo'<br>'.$ip->phones().'<br>';
+
+// echo '<br>'.$ip2->phones().'<br>';
+
+// echo'<br>'.$ip2->phones().'<br>';
+// echo '<br>'.$ip4->phones().'<br>';
+
+
+
+
+
+class  Movie{
+
+    public $name;
+    public $category;
+    public $views;
+    private $finance;
+
+   public function __construct($name,$category,$views)
+   {
+    //echo $name.''.$category.''.$views;
+    $this->name =$name;
+    $this->category=$category;
+    $this->views=$views;
+
+   }
+   public function setFinace($finance)
+   {
+        $this->finance=$finance;
+   }
+
+    }
+
+$m=new Movie("avengers:endgame","action","96 million",);
+$m->setFinace('2000000usd');
+echo '<pre>';
+var_dump($m);
+echo '</pre>';
+// $m-> name ="Avengers: Endgame ";
+// $m-> category='action';
+// $m -> views='96 million';
+// $m2=new movie();
+// $m2->name ="mission imposible";
+// $m2->category='action';
+// $m2->views="70 million";
