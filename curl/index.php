cf0e8d2b9035d1d40318c71857be4ca0fabdaece
tony555t <tonytullah764@gmail.com>
2023-10-19 15:20:43 +0300

    checking error

--- curl/index.php
+++ curl/index.php
@@ -1,11 +1,27 @@
 <?php
 
-$url = 'https://jsonplaceholder.typicode.com/users';
-// Sample example to get data.
+// Initialize cURL session
+$url = 'https://jsonplaceholder.typicode.com/posts';
 $resource = curl_init($url);
 
-// Get response status code
+// Set cURL options
+curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
 
-// set_opt_array
+// Execute the cURL request
+$result = curl_exec($resource);
 
-// Post request
+// Check for cURL errors
+if ($result === false) {
+    // An error occurred, use curl_error and curl_errno to get more details
+    $error_message = curl_error($resource);
+    $error_code = curl_errno($resource);
+    
+    // Handle the error (you can log it, display a message, etc.)
+    echo "cURL Error: $error_message (Error Code: $error_code)";
+} else {
+    // Request was successful
+    echo $result;
+}
+
+// Close the cURL session
+curl_close($resource);
